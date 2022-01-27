eca838d824dd75e2144458ad2197178a0a370231
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2022-01-27 16:40:20 -0300

    Implementing OAuth2 Authorization Server

---
+++ backend/src/main/java/com/rafaelzabotto/dscatalog/config/AuthorizationServerConfig.java
@@ -0,0 +1,51 @@
+package com.rafaelzabotto.dscatalog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Autowired
+    private JwtTokenStore jwtTokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()"); //Padrão
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("dscatalog") //Nome para quando a requisição chegar ao backend, senão não é aceito.
+                .secret(bCryptPasswordEncoder.encode("dscatalog123")) //Senha mas vamos mudar para arquivo na application
+                .scopes("read", "write")    //Permissões
+                .authorizedGrantTypes("password") //Padrão OAuth
+                .accessTokenValiditySeconds(86400); //tempo de expiração do token em segundos (1 DIA)
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(jwtTokenStore)
+                .accessTokenConverter(jwtAccessTokenConverter); //Padrão
+    }
+}
