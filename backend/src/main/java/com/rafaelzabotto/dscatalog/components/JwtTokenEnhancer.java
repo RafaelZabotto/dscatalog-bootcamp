8bbdcb8f8dc5531261d8805b9419f1c0cb80fb08
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2022-02-01 14:43:47 -0300

    Token additional information

---
+++ backend/src/main/java/com/rafaelzabotto/dscatalog/components/JwtTokenEnhancer.java
@@ -0,0 +1,41 @@
+package com.rafaelzabotto.dscatalog.components;
+
+import com.rafaelzabotto.dscatalog.entities.User;
+import com.rafaelzabotto.dscatalog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Vai acrescentar atributos personalizados no token
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
+
+        User user = userRepository.findByEmail(oAuth2Authentication.getName());
+
+        //Para acrescentar objetos no token é preciso usar um map
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userFirstName", user.getFirstName());
+        map.put("userId", user.getId());
+
+        //Cast no objeto para inserir dados no token
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) oAuth2AccessToken;
+        token.setAdditionalInformation(map);
+
+        return oAuth2AccessToken;
+
+    }
+
+}
--- backend/src/main/java/com/rafaelzabotto/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/rafaelzabotto/dscatalog/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
 package com.rafaelzabotto.dscatalog.config;
 
+import com.rafaelzabotto.dscatalog.components.JwtTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,12 +11,25 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    @Value("${jwt.duration}")
+    private Integer jwtDuration;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -28,6 +43,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtTokenEnhancer jwtTokenEnhancer;
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()"); //Padrão
@@ -35,17 +53,23 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("dscatalog") //Nome para quando a requisição chegar ao backend, senão não é aceito.
-                .secret(bCryptPasswordEncoder.encode("dscatalog123")) //Senha mas vamos mudar para arquivo na application
+        clients.inMemory().withClient(clientId) //Nome para quando a requisição chegar ao backend, senão não é aceito.
+                .secret(bCryptPasswordEncoder.encode(clientSecret))
                 .scopes("read", "write")    //Permissões
                 .authorizedGrantTypes("password") //Padrão OAuth
-                .accessTokenValiditySeconds(86400); //tempo de expiração do token em segundos (1 DIA)
+                .accessTokenValiditySeconds(jwtDuration); //tempo de expiração do token em segundos (1 DIA)
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        //Adição do tokenEnhancer passando valores personalizados
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtAccessTokenConverter, jwtTokenEnhancer));
+
         endpoints.authenticationManager(authenticationManager)
                 .tokenStore(jwtTokenStore)
-                .accessTokenConverter(jwtAccessTokenConverter); //Padrão
+                .accessTokenConverter(jwtAccessTokenConverter)
+                .tokenEnhancer(tokenEnhancerChain);
     }
 }
