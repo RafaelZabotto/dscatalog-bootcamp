62220685d5dea27cf537ea7315bb3e8547f3b86b
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2022-08-08 17:06:06 -0300

    Navbar Styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello DS Catalog</h1>
+    <Navbar/>
   );
 }
 
