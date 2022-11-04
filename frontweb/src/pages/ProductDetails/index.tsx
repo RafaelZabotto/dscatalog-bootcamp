b61cf4e6c40f31df45f0d831297d865b14586a9a
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2022-11-04 12:43:12 -0300

    Catalog-productDetails link

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,6 @@
-import { ReactComponent as ArrowIcon} from 'assets/images/Seta.svg';
+import { ReactComponent as ArrowIcon } from 'assets/images/Seta.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -8,23 +9,31 @@ const ProductDetails = () => {
     <div className="product-details-container">
       <div className="base-card product-details-card">
         <div className="goback-container">
+          <Link to="/products">
             <ArrowIcon />
-            <h2>VOLTAR</h2>
+          </Link>
+          <h2>VOLTAR</h2>
         </div>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-                <img src='https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg' alt='nome do produto'></img>
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="nome do produto"
+              ></img>
             </div>
             <div className="name-price-container">
-                <h1>Nome do Produto</h1>
-                <ProductPrice price={2345.67} />
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={2345.67} />
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
-                <h2>Descrição do Produto</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, nesciunt!</p>
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam, nesciunt!
+              </p>
             </div>
           </div>
         </div>
